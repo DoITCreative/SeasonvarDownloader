b798b18c40c4feb979a21d1cf4d27bc37ff93f5e
Ivan Rayskiy <ivanrisky@mail.ru>
2019-07-14 13:28:36 +0300

    Fill script menu

--- script_screen.cpp
+++ script_screen.cpp
@@ -7,6 +7,8 @@ Script_screen::Script_screen(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->otherLineEdit->setEnabled(false);
+    ui->plainTextEdit->setPlainText(defaultScriptHeader+"\n");
+    ui->saveScriptPushButton->setEnabled(false);
 }
 
 void Script_screen::setCalledFrom(QWidget *calledFrom)
@@ -14,9 +16,22 @@ void Script_screen::setCalledFrom(QWidget *calledFrom)
     this->calledFrom = calledFrom;
 }
 
+void Script_screen::fillScript()
+{
+    ui->plainTextEdit->setPlainText(defaultScriptHeader+"\n");
+    for (auto &token:tokens)
+    {
+        ui->plainTextEdit->setPlainText(ui->plainTextEdit->toPlainText()+selectedDownloadProgramName+" "+token.c_str()+"\n");
+    }
+}
+
 void Script_screen::setTokens(const std::vector<std::string> &tokens)
 {
     this->tokens=tokens;
+    for (auto &token:tokens)
+    {
+        ui->plainTextEdit->setPlainText(ui->plainTextEdit->toPlainText()+selectedDownloadProgramName.toStdString().c_str()+" "+token.c_str()+"\n");
+    }
 }
 
 Script_screen::~Script_screen()
@@ -33,35 +48,65 @@ void Script_screen::on_backButton_clicked()
 
 void Script_screen::on_saveLocationPushButton_clicked()
 {
-
+    QFileDialog *fd = new QFileDialog;
+    fd->setFileMode(QFileDialog::Directory);
+    fd->setOption(QFileDialog::ShowDirsOnly);
+    fd->setViewMode(QFileDialog::Detail);
+    int result = fd->exec();
+    if (result)
+    {
+        directory=fd->selectedFiles()[0];
+    }
+    ui->saveLocationLineEdit->setText(directory+"/"+defaultScriptName);
 }
 
 void Script_screen::on_saveLocationLineEdit_textChanged(const QString &arg1)
 {
-
+    if (arg1.isEmpty())
+    {
+        ui->saveScriptPushButton->setEnabled(false);
+    } else {
+        ui->saveScriptPushButton->setEnabled(true);
+    }
 }
 
 void Script_screen::on_otherLineEdit_textChanged(const QString &arg1)
 {
-
+    selectedDownloadProgramName = arg1;
+    fillScript();
 }
 
 void Script_screen::on_wgetRadioButton_clicked()
 {
     ui->otherLineEdit->setEnabled(false);
+    selectedDownloadProgramName = "wget";
+    fillScript();
 }
 
 void Script_screen::on_aria2cRadioButton_clicked()
 {
     ui->otherLineEdit->setEnabled(false);
+    selectedDownloadProgramName = "aria2c";
+    fillScript();
 }
 
 void Script_screen::on_otherRadioButton_clicked()
 {
     ui->otherLineEdit->setEnabled(true);
+    selectedDownloadProgramName = ui->otherLineEdit->text();
+    fillScript();
 }
 
 void Script_screen::on_saveScriptPushButton_clicked()
 {
-
+    QString filePath = ui->saveLocationLineEdit->text();
+    QFile file(filePath);
+    if (file.open(QIODevice::WriteOnly))
+    {
+        QTextStream stream(&file);
+        stream<<ui->plainTextEdit->toPlainText();
+        file.setPermissions(QFile::ReadUser | QFile::WriteUser | QFile::ExeUser | QFile::ReadGroup | QFile::ReadOther);
+        file.close();
+    }
+    on_backButton_clicked();
 }
--- script_screen.h
+++ script_screen.h
@@ -3,6 +3,9 @@
 #pragma once
 
 #include <QWidget>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
 #include "mainwindow.h"
 
 namespace Ui {
@@ -21,25 +24,23 @@ public:
 
 private slots:
     void on_backButton_clicked();
-
     void on_saveLocationPushButton_clicked();
-
     void on_saveLocationLineEdit_textChanged(const QString &arg1);
-
     void on_otherLineEdit_textChanged(const QString &arg1);
-
     void on_wgetRadioButton_clicked();
-
     void on_aria2cRadioButton_clicked();
-
     void on_otherRadioButton_clicked();
-
     void on_saveScriptPushButton_clicked();
 
 private:
+    void fillScript();
     Ui::Script_screen *ui;
     QWidget *calledFrom = nullptr;
     std::vector<std::string> tokens;
+    QString directory;
+    const QString defaultScriptName = "downloadScript.sh";
+    const QString defaultScriptHeader = "#!/usr/bin/env bash";
+    QString selectedDownloadProgramName = "aria2c";
 };
 
 #endif // SCRIPT_SCREEN_H
